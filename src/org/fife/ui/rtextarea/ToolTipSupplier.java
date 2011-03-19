750a05e7756645d355eb7fc89571603451672633
bobbylight <robert@fifesoft.com>
2011-03-19 13:26:45 +0000

    Fixing files that didn't include a license header.  Thanks to Vladimir Kotov!

--- src/org/fife/ui/rtextarea/ToolTipSupplier.java
+++ src/org/fife/ui/rtextarea/ToolTipSupplier.java
@@ -1,3 +1,26 @@
+/*
+ * 02/05/2009
+ *
+ * ToolTipSupplier.java - Can provide tool tips to RTextAreas without the need
+ * for subclassing.
+ * Copyright (C) 2009 Robert Futrell
+ * robert_futrell at users.sourceforge.net
+ * http://fifesoft.com/rsyntaxtextarea
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ */
 package org.fife.ui.rtextarea;
 
 import java.awt.event.MouseEvent;
