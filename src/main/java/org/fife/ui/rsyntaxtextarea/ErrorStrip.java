25e3933a1a61755ebcf39378491d7eb6afef41de
Joel Moniz <jramoniz@gmail.com>
2015-07-16 20:42:57 +0400

    Fix error marker postion errors in Error Strip

--- src/main/java/org/fife/ui/rsyntaxtextarea/ErrorStrip.java
+++ src/main/java/org/fife/ui/rsyntaxtextarea/ErrorStrip.java
@@ -306,7 +306,7 @@ public class ErrorStrip extends JComponent {
 	private int lineToY(int line) {
 		int h = textArea.getVisibleRect().height;
 		float lineCount = textArea.getLineCount();
-		return (int)(((line-1)/(lineCount-1)) * h) - 2;
+		return (int)(((line)/(lineCount-1)) * (h-2));
 	}
 
 
