db46e3a7cdba8713e6d2b106916f41630e71b081
bobbylight <robert.e.futrell@gmail.com>
2015-10-02 23:55:58 -0400

    Fixing Ctrl+/ to use right EOL comment for NSIS.  Adding some unit tests for NSIS and Delphi highlighting

---
+++ src/test/java/org/fife/ui/rsyntaxtextarea/modes/DelphiTokenMakerTest.java
@@ -0,0 +1,448 @@
+/*
+ * 03/12/2015
+ *
+ * This library is distributed under a modified BSD license.  See the included
+ * RSyntaxTextArea.License.txt file for details.
+ */
+package org.fife.ui.rsyntaxtextarea.modes;
+
+import javax.swing.text.Segment;
+
+import org.fife.ui.rsyntaxtextarea.Token;
+import org.fife.ui.rsyntaxtextarea.TokenMaker;
+import org.fife.ui.rsyntaxtextarea.TokenTypes;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit tests for the {@link DelphiTokenMaker} class.
+ *
+ * @author Robert Futrell
+ * @version 1.0
+ */
+public class DelphiTokenMakerTest extends AbstractTokenMakerTest {
+
+
+	/**
+	 * Returns a new instance of the <code>TokenMaker</code> to test.
+	 *
+	 * @return The <code>TokenMaker</code> to test.
+	 */
+	private TokenMaker createTokenMaker() {
+		return new DelphiTokenMaker();
+	}
+
+
+	@Test
+	public void testDataTypes() {
+
+		String[] dataTypes = {
+			"shortint",
+			"byte",
+			"char",
+			"smallint",
+			"integer",
+			"word",
+			"longint",
+			"cardinal",
+			"boolean",
+			"bytebool",
+			"wordbool",
+			"longbool",
+			"real",
+			"single",
+			"double",
+			"extended",
+			"comp",
+			"currency",
+			"pointer",
+		};
+
+		for (String code : dataTypes) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.DATA_TYPE, token.getType());
+		}
+
+	}
+
+
+	@Test
+	public void testEolComments() {
+
+		String[] eolCommentLiterals = {
+			"// Hello world",
+		};
+
+		for (String code : eolCommentLiterals) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.COMMENT_EOL, token.getType());
+		}
+
+	}
+
+
+	@Test
+	public void testEolComments_URL() {
+
+		String[] eolCommentLiterals = {
+			"// Hello world http://www.sas.com",
+		};
+
+		for (String code : eolCommentLiterals) {
+
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.COMMENT_EOL, token.getType());
+
+			token = token.getNextToken();
+			Assert.assertTrue(token.isHyperlink());
+			Assert.assertEquals(TokenTypes.COMMENT_EOL, token.getType());
+			Assert.assertEquals("http://www.sas.com", token.getLexeme());
+
+		}
+
+	}
+
+
+	@Test
+	public void testFloatingPointLiterals() {
+
+		String code =
+			// Basic doubles
+			"3.0 4.2 3.0 4.2 .111 " +
+			// Basic floats ending in f, F, d, or D
+			"3.f 3.F 3.0f 3.0F .111f .111F " +
+			// lower-case exponent, no sign
+			"3.e7f 3.e7F 3.0e7f 3.0e7F .111e7f .111e7F " +
+			// Upper-case exponent, no sign
+			"3.E7f 3.E7F 3.0E7f 3.0E7F .111E7f .111E7F " +
+			// Lower-case exponent, positive
+			"3.e+7f 3.e+7F 3.0e+7f 3.0e+7F .111e+7f .111e+7F " +
+			// Upper-case exponent, positive
+			"3.E+7f 3.E+7F 3.0E+7f 3.0E+7F .111E+7f .111E+7F " +
+			// Lower-case exponent, negative
+			"3.e-7f 3.e-7F 3.0e-7f 3.0e-7F .111e-7f .111e-7F " +
+			// Upper-case exponent, negative
+			"3.E-7f 3.E-7F 3.0E-7f 3.0E-7F .111E-7f .111E-7F";
+
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+
+		String[] keywords = code.split(" +");
+		for (int i = 0; i < keywords.length; i++) {
+			Assert.assertEquals(keywords[i], token.getLexeme());
+			Assert.assertEquals("Invalid floating point: " + token, TokenTypes.LITERAL_NUMBER_FLOAT, token.getType());
+			if (i < keywords.length - 1) {
+				token = token.getNextToken();
+				Assert.assertTrue("Not a whitespace token: " + token, token.isWhitespace());
+				Assert.assertTrue(token.is(TokenTypes.WHITESPACE, " "));
+			}
+			token = token.getNextToken();
+		}
+
+		Assert.assertTrue(token.getType() == TokenTypes.NULL);
+
+	}
+
+
+	@Test
+	public void testHexLiterals() {
+
+		String code = "0x1 0xfe 0x333333333333 0X1 0Xfe 0X33333333333 0xFE 0XFE " +
+				"0x1l 0xfel 0x333333333333l 0X1l 0Xfel 0X33333333333l 0xFEl 0XFEl " +
+				"0x1L 0xfeL 0x333333333333L 0X1L 0XfeL 0X33333333333L 0xFEL 0XFEL";
+
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+
+		String[] keywords = code.split(" +");
+		for (int i = 0; i < keywords.length; i++) {
+			Assert.assertEquals(keywords[i], token.getLexeme());
+			Assert.assertEquals("Invalid hex literal: " + token, TokenTypes.LITERAL_NUMBER_HEXADECIMAL, token.getType());
+			if (i < keywords.length - 1) {
+				token = token.getNextToken();
+				Assert.assertTrue("Not a whitespace token: " + token, token.isWhitespace());
+				Assert.assertTrue(token.is(TokenTypes.WHITESPACE, " "));
+			}
+			token = token.getNextToken();
+		}
+
+		Assert.assertTrue(token.getType() == TokenTypes.NULL);
+
+	}
+
+
+	@Test
+	public void testStandardFunctions() {
+
+		String[] functions = {
+				"absolute",
+				"abstract",
+				"assembler",
+				"automated",
+				"cdecl",
+				"contains",
+				"default",
+				"deprecated",
+				"dispid",
+				"dynamic",
+				"export",
+				"external",
+				"far",
+				"forward",
+				"implements",
+				"index",
+				"library",
+				"local",
+				"message",
+				"name",
+				"namespaces",
+				"near",
+				"nil",
+				"nodefault",
+				"overload",
+				"override",
+				"package",
+				"pascal",
+				"platform",
+				"private",
+				"protected",
+				"public",
+				"published",
+				"read",
+				"readonly",
+				"register",
+				"reintroduce",
+				"requires",
+				"resident",
+				"safecall",
+				"self",
+				"stdcall",
+				"stored",
+				"varargs",
+				"virtual",
+				"write",
+				"writeonly",
+		};
+
+		for (String code : functions) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.FUNCTION, token.getType());
+		}
+
+	}
+
+
+	@Test
+	public void testKeywords() {
+
+		String[] words = {
+			"array",
+			"as",
+			"at",
+			"asm",
+			"begin",
+			"case",
+			"class",
+			"const",
+			"constructor",
+			"destructor",
+			"dispinterface",
+			"div",
+			"do",
+			"downto",
+			"else",
+			"end",
+			"except",
+			"exports",
+			"file",
+			"final",
+			"finalization",
+			"finally",
+			"for",
+			"function",
+			"goto",
+			"if",
+			"implementation",
+			"in",
+			"inherited",
+			"initialization",
+			"inline",
+			"interface",
+			"is",
+			"label",
+			"mod",
+			"not",
+			"object",
+			"of",
+			"on",
+			"or",
+			"out",
+			"packed",
+			"procedure",
+			"program",
+			"property",
+			"raise",
+			"record",
+			"repeat",
+			"resourcestring",
+			"set",
+			"sealed",
+			"shl",
+			"shr",
+			"static",
+			"string",
+			"then",
+			"threadvar",
+			"to",
+			"try",
+			"type",
+			"unit",
+			"unsafe",
+			"until",
+			"uses",
+			"var",
+			"while",
+			"with",
+			"xor",
+		};
+
+		for (String code : words) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals("Not a keyword: " + token, TokenTypes.RESERVED_WORD, token.getType());
+		}
+
+	}
+
+
+	@Test
+	public void testMultiLineComments() {
+
+		String[] mlcLiterals = {
+			"{Hello world }",
+		};
+
+		for (String code : mlcLiterals) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.COMMENT_MULTILINE, token.getType());
+		}
+
+	}
+
+
+	@Test
+	public void testMultiLineComments_URL() {
+
+		String[] mlcLiterals = {
+			"{ Hello world http://www.sas.com }",
+		};
+
+		for (String code : mlcLiterals) {
+
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals(TokenTypes.COMMENT_MULTILINE, token.getType());
+
+			token = token.getNextToken();
+			Assert.assertTrue(token.isHyperlink());
+			Assert.assertEquals(TokenTypes.COMMENT_MULTILINE, token.getType());
+			Assert.assertEquals("http://www.sas.com", token.getLexeme());
+
+			token = token.getNextToken();
+			Assert.assertEquals(TokenTypes.COMMENT_MULTILINE, token.getType());
+			Assert.assertEquals(" }", token.getLexeme());
+
+		}
+
+	}
+
+
+	@Test
+	public void testOperators() {
+
+		String code = "^ @ : = < > + - / *";
+
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+
+		String[] operators = code.split(" +");
+		for (int i = 0; i < operators.length; i++) {
+			Assert.assertEquals(operators[i], token.getLexeme());
+			Assert.assertEquals(TokenTypes.OPERATOR, token.getType());
+			if (i < operators.length - 1) {
+				token = token.getNextToken();
+				Assert.assertTrue("Not a whitespace token: " + token, token.isWhitespace());
+				Assert.assertTrue("Not a single space: " + token, token.is(TokenTypes.WHITESPACE, " "));
+			}
+			token = token.getNextToken();
+		}
+
+		Assert.assertTrue(token.getType() == TokenTypes.NULL);
+
+	}
+
+
+	@Test
+	public void testSeparators() {
+
+		String code = "( ) [ ]";
+
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+
+		String[] separators = code.split(" +");
+		for (int i = 0; i < separators.length; i++) {
+			Assert.assertEquals(separators[i], token.getLexeme());
+			Assert.assertEquals(TokenTypes.SEPARATOR, token.getType());
+			// Just one extra test here
+			Assert.assertTrue(token.isSingleChar(TokenTypes.SEPARATOR, separators[i].charAt(0)));
+			if (i < separators.length - 1) {
+				token = token.getNextToken();
+				Assert.assertTrue("Not a whitespace token: " + token, token.isWhitespace());
+				Assert.assertTrue("Not a single space: " + token, token.is(TokenTypes.WHITESPACE, " "));
+			}
+			token = token.getNextToken();
+		}
+
+		Assert.assertTrue(token.getType() == TokenTypes.NULL);
+
+	}
+
+
+	@Test
+	public void testStringLiterals() {
+
+		String[] stringLiterals = {
+			"''", "'hi'",
+		};
+
+		for (String code : stringLiterals) {
+			Segment segment = createSegment(code);
+			TokenMaker tm = createTokenMaker();
+			Token token = tm.getTokenList(segment, TokenTypes.NULL, 0);
+			Assert.assertEquals("Not a string: " + token, TokenTypes.LITERAL_STRING_DOUBLE_QUOTE, token.getType());
+		}
+
+	}
+
+
+}
