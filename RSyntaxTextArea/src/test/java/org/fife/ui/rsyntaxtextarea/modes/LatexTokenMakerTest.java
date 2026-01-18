f8ccc85a4eb8b38afe3b11ba59cc28434c87f705
sesquialtera87 <sesquialtera87@gmail.com>
2026-01-18 11:37:49 +0100

    Test improvements and some fix in the scanner

--- RSyntaxTextArea/src/test/java/org/fife/ui/rsyntaxtextarea/modes/LatexTokenMakerTest.java
+++ RSyntaxTextArea/src/test/java/org/fife/ui/rsyntaxtextarea/modes/LatexTokenMakerTest.java
@@ -14,12 +14,16 @@ import org.fife.ui.rsyntaxtextarea.TokenTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Unit tests for the {@link LatexTokenMaker} class.
  *
  * @author Robert Futrell
- * @version 1.0
+ * @author Mattia Marelli
+ * @version 1.1
  */
 class LatexTokenMakerTest extends AbstractJFlexTokenMakerTest {
 
@@ -71,6 +75,108 @@ class LatexTokenMakerTest extends AbstractJFlexTokenMakerTest {
 		token = token.getNextToken();
 		Assertions.assertEquals(TokenTypes.REGEX, token.getType());
 	}
+
+
+	@Test
+	void testMultilineOptions() {
+		String code = "\\usepackage[a,b";
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token tk = tm.getTokenList(segment, TokenTypes.NULL, 0);
+		Assertions.assertEquals(TokenTypes.RESERVED_WORD, tk.getType());
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "["));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.PREPROCESSOR, "a"));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.OPERATOR, ","));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.PREPROCESSOR, "b"));
+
+		code = "c]{package}";
+		segment = createSegment(code);
+		tk = tm.getTokenList(segment, LatexTokenMaker.STATE_OPT, 0);
+		Assertions.assertTrue(tk.is(TokenTypes.PREPROCESSOR, "c"));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "]"));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "{"));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.REGEX, "package"));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "}"));
+	}
+
+
+	void testMathEnv(String markStart, String markEnd) {
+		String code = String.format("%s\\alpha%s", markStart, markEnd);
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token tk = tm.getTokenList(segment, TokenTypes.NULL, 0);
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, markStart));
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.is(TokenTypes.FUNCTION, "\\alpha"));
+
+		tk = tk.getNextToken();
+		Assertions.assertEquals(1, tk.getLanguageIndex()); // math
+		Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, markEnd));
+	}
+
+
+	@Test
+	void testMath() {
+		List.of(
+			Map.entry("$", "$"),
+			Map.entry("$$", "$$"),
+			Map.entry("\\[", "\\]"),
+			Map.entry("\\(", "\\)")
+		).forEach(e -> testMathEnv(e.getKey(), e.getValue()));
+	}
+
+
+	@Test
+	void testHyperlink() {
+		String code = "% https://google.com";
+		Segment segment = createSegment(code);
+		TokenMaker tm = createTokenMaker();
+		Token tk = tm.getTokenList(segment, TokenTypes.NULL, 0);
+		Assertions.assertTrue(tk.isComment());
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.isComment());
+
+		tk = tk.getNextToken();
+		Assertions.assertTrue(tk.isHyperlink());
+	}
+
+
+	@Test
+	void testSpecialCommands() {
+		TokenMaker tm = createTokenMaker();
+		List.of("newcommand", "renewcommand", "newenvironment", "renewenvironment",
+				"newcounter", "newlength", "def", "usepackage")
+			.forEach(s -> {
+				Segment segment = createSegment("\\" + s + "{}");
+				Token tk = tm.getTokenList(segment, TokenTypes.NULL, 0);
+				Assertions.assertTrue(tk.is(TokenTypes.RESERVED_WORD, "\\" + s));
+
+				tk = tk.getNextToken();
+				Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "{"));
+
+				tk = tk.getNextToken();
+				Assertions.assertTrue(tk.is(TokenTypes.SEPARATOR, "}"));
+			});
+	}
+
 
 	@Test
 	void testLabel() {
